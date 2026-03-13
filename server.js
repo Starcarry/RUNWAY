159519fcb85b9cff492270d831b7ece0723bba98
Starcarry <Vitordsfw@gmail.com>
2026-03-13 12:27:41 -0300

    Update server.js

--- server.js
+++ server.js
@@ -521,5 +521,5 @@ app.post("/wati/webhook", async (req, res) => {
 
 const PORT = Number(process.env.PORT) || 3000;
 app.listen(PORT, "0.0.0.0", () => {
-  console.log(\`Servidor rodando na porta \${PORT}\`);
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
